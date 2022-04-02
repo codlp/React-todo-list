6761dd722c4ce20c2c8363a6ad5be32df456a327
coralie delpha <coralie.delpha@gmail.com>
2022-04-02 17:52:45 +0100

    create TodoList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
-
+  useState([])
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add a task</button>
+      <button>Clear completed tasks</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
